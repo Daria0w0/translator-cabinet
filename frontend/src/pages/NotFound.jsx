69e81d1ae4cf41709fd2940657c4a4b41f0a0486
Daria0w0 <niko.roo77@gmail.com>
2025-09-28 20:58:04 +0300

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <div>
+            <h2>Страница не найдена</h2>
+        </div>
+  );
+}
---
+++ frontend/src/pages/User.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function User() {
+  return (
+    <div>
+      <h2>Личный кабинет</h2>
+      <p>Это личный кабинет переводчика или редактора.</p>
+    </div>
+  );
+}
